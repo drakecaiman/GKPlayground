3dd42d1847622d18af6f953a5a7c443afa0282d1
Duncan Oliver <duncan.oliver@gmail.com>
2019-02-06 01:10:01 -0600

    Initial Commit

---
+++ GKPlayground/GKPlayground.h
@@ -0,0 +1,19 @@
+//
+//  GKPlayground.h
+//  GKPlayground
+//
+//  Created by Duncan on 2/6/19.
+//  Copyright © 2019 Duncan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GKPlayground.
+FOUNDATION_EXPORT double GKPlaygroundVersionNumber;
+
+//! Project version string for GKPlayground.
+FOUNDATION_EXPORT const unsigned char GKPlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKPlayground/PublicHeader.h>
+
+
