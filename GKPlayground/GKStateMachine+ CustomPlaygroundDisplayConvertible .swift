f0cc8bb79a841f42307ae7c54eb6be47ad164935
Duncan Oliver <duncan.oliver@gmail.com>
2019-02-10 08:44:34 -0600

    Initial work for GKState array view

---
+++ GKPlayground/GKStateMachine+ CustomPlaygroundDisplayConvertible .swift
@@ -0,0 +1,169 @@
+//
+//  GKStateMachine+ CustomPlaygroundDisplayConvertible .swift
+//  GKPlayground
+//
+//  Created by Duncan on 2/6/19.
+//  Copyright © 2019 Duncan. All rights reserved.
+//
+
+import GameplayKit
+
+
+extension String
+{
+    public func cgGlyphs(forFont font: NSFont) -> [CGGlyph]
+    {
+        var glyphs = [CGGlyph]()
+        
+        for c in self
+        {
+            let nextGlyph = font.glyph(withName: String(c))
+            glyphs.append(CGGlyph(nextGlyph))
+        }
+        
+        return glyphs
+    }
+}
+
+extension GKState// : CustomPlaygroundDisplayConvertible
+{
+    public var playgroundDescription : Any
+    {
+        let stateRect = NSRect(origin: CGPoint.zero, size: NodeView.defaultNodeSize)
+        let stateNodeView = NodeView(frame: stateRect)
+        stateNodeView.name = String(describing: type(of: self))
+        
+        return stateNodeView
+    }
+    
+    // TODO: move 'playgroundDescription' here?
+    public var stateView : NSView
+    {
+        return NSView()
+    }
+}
+
+extension GKStateMachine// : CustomPlaygroundDisplayConvertible
+{
+    public var playgroundDescription: Any
+    {
+        return NSView()
+    }
+}
+
+extension Array : CustomPlaygroundDisplayConvertible where Element == GKState
+{
+    public var playgroundDescription : Any
+    {
+//      Get connections between states
+        var connections = [String : Set<String>]()
+        for currentState in self
+        {
+            let currentStateName = String(describing: type(of: currentState))
+            for outState in self
+            {
+                guard currentState.isValidNextState(type(of: outState)) else { continue }
+                let outStateName = String(describing: type(of: outState))
+                if connections[currentStateName] == nil
+                {
+                    connections[currentStateName] = Set<String>()
+                }
+                connections[currentStateName]?.insert(outStateName)
+            }
+        }
+        
+//      Get views
+        let viewRect = NSRect(x: 0, y: 0, width: 200, height: 200)
+        let view = NSView(frame: viewRect)
+        view.wantsLayer = true
+        view.layer?.backgroundColor = .black
+//        view.translatesAutoresizingMaskIntoConstraints = true
+//        view.autoresizesSubviews = false
+        
+        
+        let nodeSpacing : CGFloat = 25.0
+        var nodeOrigin = CGPoint.zero
+//        var nodeOrigin = CGPoint(x: 0.0, y: view.frame.height - nodeSize.height)
+        let arrowheadLength : CGFloat = 8.0
+        
+        for nextState in self
+        {
+            let nextStateName = String(describing: type(of: nextState))
+            let nextViewFrame = NSRect(origin: nodeOrigin, size: NodeView.defaultNodeSize)
+            let nextView = NodeView(frame: nextViewFrame)
+            nextView.name = String(nextStateName.prefix(1))
+//            nextView.autoresizingMask = [.maxXMargin, .minYMargin]
+            
+            view.addSubview(nextView)
+            nodeOrigin.x += 25.0
+//            nodeOrigin.x = nextViewFrame.maxX
+//            nodeOrigin.x += nodeSize.width + nodeSpacing
+//            nodeOrigin.y -= nodeSize.height
+            
+            if false
+            {
+//          Draw arrows
+            if var nextStateTransistions = connections[nextStateName]
+            {
+                let nextPath = NSBezierPath()
+                if nextStateTransistions.contains(nextStateName)
+                {
+//                    let selfArrowStart  = CGPoint(x: nextPath.bounds.minX, y: nextPath.bounds.midY)
+//                    let selfArrowCenter = CGPoint(x: nextPath.bounds.minX, y: nextPath.bounds.maxY)
+//                    let selfArrowEnd    = CGPoint(x: nextPath.bounds.minX + nextPath.bounds.midY,
+//                                                  y: nextPath.bounds.maxY)
+                    let selfArrowCenter = CGPoint(x: nextPath.bounds.minX, y: nextPath.bounds.maxY)
+                    let arrowPath = NSBezierPath()
+                    arrowPath.appendArc(withCenter: selfArrowCenter,
+                                        radius: (nextPath.bounds.maxY - nextPath.bounds.midY),
+                                        startAngle: 270.0,
+                                        endAngle: 0,
+                                        clockwise: true)
+                    let arrowEnd = arrowPath.currentPoint
+                    arrowPath.relativeLine(to: NSPoint(x: arrowheadLength * cos(CGFloat.pi / 4),
+                                                       y: arrowheadLength * sin(CGFloat.pi / 4)))
+                    arrowPath.move(to: arrowEnd)
+                    arrowPath.relativeLine(to: NSPoint(x: -arrowheadLength * cos(CGFloat.pi / 4),
+                                                       y: arrowheadLength * sin(CGFloat.pi / 4)))
+                    
+                    nextStateTransistions.remove(nextStateName)
+                }
+                for nextExitState in nextStateTransistions
+                {
+                    let nextArrowStart = CGPoint(x: nextPath.bounds.maxX,
+                                                 y: nextPath.bounds.midY)
+                    let arrowPath = NSBezierPath()
+                    arrowPath.move(to: nextArrowStart)
+                    arrowPath.relativeLine(to: CGPoint(x: nodeSpacing,
+                                                       y: 0.0))
+                    let arrowEnd = arrowPath.currentPoint
+                    arrowPath.relativeLine(to: NSPoint(x: -arrowheadLength * cos(CGFloat.pi / 4),
+                                                       y: arrowheadLength * sin(CGFloat.pi / 4)))
+                    arrowPath.move(to: arrowEnd)
+                    arrowPath.relativeLine(to: NSPoint(x: -arrowheadLength * cos(CGFloat.pi / 4),
+                                                       y: -arrowheadLength * sin(CGFloat.pi / 4)))
+                    if connections[nextExitState]?.contains(nextStateName) ?? false
+                    {
+                        arrowPath.move(to: nextArrowStart)
+                        arrowPath.relativeLine(to: NSPoint(x: arrowheadLength * cos(CGFloat.pi / 4),
+                                                           y: arrowheadLength * sin(CGFloat.pi / 4)))
+                        arrowPath.move(to: nextArrowStart)
+                        arrowPath.relativeLine(to: NSPoint(x: arrowheadLength * cos(CGFloat.pi / 4),
+                                                           y: -arrowheadLength * sin(CGFloat.pi / 4)))
+                        
+//                      Remove state
+                        connections[nextExitState]?.remove(nextStateName)
+                    }
+                }
+                if nextStateTransistions.count > 0
+                {
+                    
+                }
+            }
+            }
+        }
+        
+//        return path
+        return view
+    }
+}
---
+++ GKPlayground/TestPlayground.playground/Contents.swift
@@ -0,0 +1,55 @@
+import Cocoa
+import GameplayKit
+import PlaygroundSupport
+import GKPlayground
+
+// Define some example states
+class AllState : GKState {}
+class NoneState : GKState
+{
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        return false
+    }
+}
+class SelfState : GKState
+{
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        return stateClass == type(of: self)
+    }
+}
+class OneWayState : GKState
+{
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool
+    {
+        switch stateClass
+        {
+        case is NoneState.Type:
+            fallthrough
+        case is SelfState.Type:
+            return true
+        default:
+            return false
+        }
+    }
+}
+class TwoWayState : GKState {}
+
+//class SelfOneWayState : GKState {}
+
+let states = [AllState(),
+              NoneState(),
+              SelfState(),
+              OneWayState()]
+
+if let view = (states.playgroundDescription as? NSView)
+{
+    for next in view.subviews
+    {
+        print(next.frame)
+    }
+}
+
+//PlaygroundPage.current.needsIndefiniteExecution = true
+//PlaygroundPage.current.liveView = states.playgroundDescription as? NodeView
