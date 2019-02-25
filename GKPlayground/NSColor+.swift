35714867aeb38f321c327fd7169529a44e982f30
Duncan Oliver <duncan.oliver@gmail.com>
2019-02-25 06:12:57 -0600

    Cleanup for alignment

--- GKPlayground/NSColor+.swift
+++ GKPlayground/NSColor+.swift
@@ -13,8 +13,8 @@ extension NSColor
     public convenience init(fromInt int: Int)
     {
         let red     = CGFloat((int & (0xff << 16)) >> 16) / 255.0
-        let green   = CGFloat((int & (0xff << 8)) >> 8) / 255.0
-        let blue    = CGFloat((int & (0xff << 0)) >> 0) / 255.0
+        let green   = CGFloat((int & (0xff << 08)) >> 08) / 255.0
+        let blue    = CGFloat((int & (0xff << 00)) >> 00) / 255.0
         self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
